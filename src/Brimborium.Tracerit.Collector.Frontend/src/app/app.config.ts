fcff6a3a028345e300faa1c12eb3f9f66b1be91a
Florian Grimm <2593674+FlorianGrimm@users.noreply.github.com>
2025-11-10 18:18:21 +0100

    timeruler

--- src/Brimborium.Tracerit.Collector.Frontend/src/app/app.config.ts
+++ src/Brimborium.Tracerit.Collector.Frontend/src/app/app.config.ts
@@ -4,10 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
         provideZoneChangeDetection({ eventCoalescing: true }), 
         provideRouter(routes),
-        provideHttpClient(withFetch())
+        provideHttpClient(withFetch()),
+        provideAnimationsAsync(),
+         providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
     ]
 };
